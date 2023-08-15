9d1e4fb824f7be54fa41a28ab3e2976b6b4acd5b
Hemant2335 <knrt73373@gmail.com>
2023-08-15 13:00:33 +0530

    minor_Change

--- oneprint/backend/index.js
+++ oneprint/backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-var cors = require('cors');
+const cors = require('cors');
 require('dotenv').config(); // This loads the environment variables from .env
 
 const app = express();
